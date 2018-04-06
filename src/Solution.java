57b21207e4f1af88cd5c4c710d37de904597e90b
mglosecki <michael.glosecki@gmail.com>
2018-04-05 23:32:21 -0500

    Stopped doing math

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 class Solution {
     public String convert(String s, int numRows) {
+        List<Queue<Character>> arrayListOfQueues = new ArrayList<>();
         StringBuilder zigzagString = new StringBuilder();
 
-        int zag = numRows / 2;
-        int nextCharacter = numRows + zag;
-        int nextRow = 0;
-        while (zigzagString.length() < s.length()) {
-            for (int i = nextRow; i < s.length(); i += nextCharacter) {
-                zigzagString.append(s.charAt(i));
+        Queue<Character> row;
+        int currentRow = -1;
+        boolean zag = false;
+        for (int index = 0; index < s.length(); index++) {
+            if (zag) {
+                currentRow--;
+                zag = currentRow != 0;
+            } else {
+                currentRow++;
+                zag = currentRow == numRows - 1;
             }
-            nextCharacter -= zag;
-            if (nextCharacter < zag) {
-                nextCharacter = numRows + zag;
+            if(arrayListOfQueues.size() != numRows){
+                row = new ArrayDeque<>();
+                row.add(s.charAt(index));
+                arrayListOfQueues.add(row);
+            } else {
+                row = arrayListOfQueues.get(currentRow);
+                row.add(s.charAt(index));
             }
-            nextRow++;
+        }
+
+        for (Queue<Character> currentRowFromArray : arrayListOfQueues) {
+            while (!currentRowFromArray.isEmpty()) {
+                zigzagString.append(currentRowFromArray.remove());
+            }
         }
 
         return zigzagString.toString();
@@ -22,6 +41,6 @@ class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println("Correct Answer: PAHNAPLSIIGYIR");
-        System.out.println("Calculated Answer: " + solution.convert("PAYPALISHIRING", 3));
+        System.out.println("Calculated Answer: " + solution.convert("PAYPALISHIRING", 4));
     }
 }
